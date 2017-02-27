8a23b81e08d681651a35e72684400f9dd63eed95
AudunO96 <audiso96@gmail.com>
2017-02-27 11:59:54 +0100

    Started project!

---
+++ Source/PugsInSpace/PugsInSpace.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PugsInSpace.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PugsInSpace, "PugsInSpace" );
